457e4ee3692cc66b3a2fa89f314732204cffd736
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-12 17:36:57 -0300

    fix: fix type folder path

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 // Exportação dos tipos para serem usados em outras partes do projeto
 // Padronização de exportação de tipos para facilitar a manutenção e a escalabilidade do código
-export type { Product, Category, CategorySlug } from "./product";
+export type { Product, Category, CategorySlug } from "./Product";
 export type { CartItem, CartStore } from "./cart";
 export type { CheckoutFormData } from "./checkout";

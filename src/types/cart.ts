a4c94a1640a3c1c045185af241f53b090c9b72c7
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-12 17:40:46 -0300

    fix: fix type folder path

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -1,4 +1,4 @@
-import type { Product } from "./product";
+import type { Product } from "./Product";
 
 // Tipo do item do carrinho - representa um produto e sua quantidade
 export interface CartItem {

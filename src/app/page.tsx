5bcd547a90117cc09b8b18ebdf9322d52d138f06
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-15 17:37:30 -0300

    style: basic hero styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,32 @@ import { PackageIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-      <section>
-        <div>
-          <div className="w-screen">
-            <Image
-              src="/images/hero.webp"
-              alt="Hero image"
-              width={1200}
-              height={630}
-            />
-          </div>
-          <div />
-          <div>
-            <span>DevStore, </span>
-            <span> a place for developers.</span>
-            <p>Tudo para seu setup!</p>
-            <p>Periféricos e acessórios tech com os melhores preços</p>
-          </div>
-          <div>
-            <Link href="/products">
-              <Button>
-                Explore nossos produtos <PackageIcon size={24} />
-              </Button>
-            </Link>
-          </div>
+    <section className="relative flex align-center justify-center direction-col min-h-80vh text-center">
+      <div>
+        <div className="absolute inset-0 w-full h-full">
+          <Image
+            src="/images/hero.webp"
+            alt="Hero image"
+            width={1200}
+            height={630}
+            className="w-full h-full object-cover object-center block"
+          />
         </div>
-      </section>
-    </div>
+        <div className="absolute inset-0 bg-black opacity-50" />
+        <div className="relative z-2 text-white max-w-2xl flex direction-col align-center g-4 p-4 rounded">
+          <span>DevStore, </span>
+          <span> a place for developers.</span>
+          <p>Tudo para seu setup!</p>
+          <p>Periféricos e acessórios tech com os melhores preços</p>
+        </div>
+        <div>
+          <Link href="/products">
+            <Button>
+              Explore nossos produtos <PackageIcon size={24} />
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
